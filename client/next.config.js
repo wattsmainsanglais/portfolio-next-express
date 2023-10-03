48ca759bdb9a17d94f3b3cedd5b62d57a652cc6b
mainsanglais <wattsmainsanglais@gmail.com>
2023-10-03 21:05:53 +0200

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+   
+     async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/express/contact",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+     }, 
+     
     async rewrites() {
         return [
             // rewritting API requests to Express server
@@ -9,22 +25,8 @@ const nextConfig = {
                 destination: "https://server-production-7c47.up.railway.app/api/express"
             },
         ];
-    },
+    }
 
-     async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/express",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://server-production-7c47.up.railway.app/api/express/contact" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-     }  
 };
 
 module.exports = nextConfig
