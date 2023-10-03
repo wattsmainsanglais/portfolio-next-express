8261cb543a17e0d60f37978326ec3cde6279f1d5
wattsmainsanglais <wattsmainsanglais@gmail.com>
2023-10-03 20:20:19 +0200

    add security policy to express server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,26 @@ const {handleContactForm} = require('./utils/handleContact')
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+      "default-src 'self'; font-src 'self' https://fonts.gstatic.com ; img-src 'self' 'unsafe-inline' ; script-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com  ; style-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com; frame-src 'self'; connect-src http://localhost:5000  'https://server-production-7c47.up.railway.app/api/express/contact' ; worker-src blob:; child-src blob:"
+  );
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+
+
+  const allowedOrigins = ['https://server-production-7c47.up.railway.app/api/express/contact', 'http://localhost:5000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(bodyParser.text());
 app.use(cors())
 
