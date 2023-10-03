6f61f5929c5267d1405e005c66443cde67b4c41e
mainsanglais <wattsmainsanglais@gmail.com>
2023-10-03 20:50:36 +0200

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
                 source: "/api/express",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://server-production-7c47.up.railway.app/" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: "https://server-production-7c47.up.railway.app/api/express/contact" }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
