0a2aa5766a20317807c1e3c4455b7533c81d991b
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-09-24 22:23:46 +0200

    change a single project details, update npm. Will need to update to Nextjs14 eventually

--- client/app/_components/projects.js
+++ client/app/_components/projects.js
@@ -1,6 +1,6 @@
 import SingleProject from "./single-projects/singleProject"
 
-import ndThumb from '../../public/images/naturedopesthumb.png'
+import ndThumb from '../../public/images/nd-background.jpg'
 import popupThumb from '../../public/images/popweddingThumb.png'
 import ptcThumb from '../../public/images/PTC.png'
 import ndFinder from '../../public/images/nd-flower-finder.png'
@@ -14,7 +14,7 @@ export default function Projects(){
 
              <SingleProject link="https://www.naturedopes.com/" heading='Nature Dopes' src={ndThumb} alt='Nature Dopes Thumbnail' desc="Nature Dopes 
              is an ongoing project dedicated to creating a site firstly for the appreciation of natural flora, secondly as an educational tool. 
-             To preserve the environment, we first need to understand it. It's a full stack application developed in HTML, CSS, NodeJS/ExpressJS & postgreSQL. 
+             To preserve the environment, we first need to understand it. It's a full stack application developed in NextJs 14 Typescipt, It utilizes many of the latest NextJs tools, such as Next-auth for authentification, Next-intl for multi language, Radix Ui, Iagon for decentralized storage and Prisma for Database management.
              Follow the link to view" /> 
 
             <SingleProject link="https://www.thepopupweddingcreche.fr/" heading='The Popup Wedding Creche' src={popupThumb} alt='The pop up wedding creche thumbnail' desc="This is the homepage page for a new startup business offering childcare options for weddings and large events, please give them a look if you have any major events planned in South West France. 
