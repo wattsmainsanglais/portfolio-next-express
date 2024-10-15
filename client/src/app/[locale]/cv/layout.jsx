cfa5dc5d86861930deb4e8342c7a82e89d034505
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-10-15 10:38:33 +0200

    added radix themes, created sidebar and content with active state mechanism

--- client/src/app/[locale]/cv/layout.jsx
+++ client/src/app/[locale]/cv/layout.jsx
@@ -7,18 +7,21 @@ import profilePic from '../../../../public/images/profilebw.jpg'
 import linkedIn from '../../../../public/images/linkedin_socialnetwork_17441.png'
 import gitHub from '../../../../public/images/github_original_wordmark_logo_icon_146506.png'
 
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 export default function CvRoot({children}){
 
     return(
 
-        <html lang="en" >
-
-            <body>
+   
+          
+            <body style={{backgroundColor: 'black'}}>
+                  <Theme>
                 {children}
-                <p>This will be the cv</p>
+                 </Theme>
             </body>
-
-        </html>
+            
+  
     )
 }
