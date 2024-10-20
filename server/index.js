ff9aef2fae01b11559f1c7dfaed176b45082b670
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-10-20 10:19:06 +0200

    return Cors policy zith updqted allowed origins

--- server/index.js
+++ server/index.js
@@ -15,13 +15,13 @@ app.use(function (req, res, next) {
   res.setHeader('X-Content-Type-Options', 'nosniff');
 
 
-  /*const allowedOrigins = ['https://server-production-7c47.up.railway.app/api/express/contact', 'https://portfolio-next-express-production.up.railway.app', 'http://localhost:5000'];
+  const allowedOrigins = ['https://server-production-7c47.up.railway.app/api/express/contact', 'https://portfolio-next-express-production.up.railway.app', 'http://localhost:5000', 'http://localhost:3000', 'https://www.awattsdev.eu/'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-       
-  }*/
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
+ 
   res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
